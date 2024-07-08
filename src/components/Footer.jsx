c3dca60dc7ceb9f506dc482d79bb7a025867c74f
johnkennedyb <113602917+johnkennedyb@users.noreply.github.com>
2024-07-07 21:44:43 -1200

    First Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+export default function Footer() {
+    
+    return (
+        <div className="bg-dark" style={{padding: "10px 0px", backgroundColor : "#######", textAlign: 'center', position:'fixed', width: '100%',height : '58px', bottom : '0', left: '0', right: '0', zIndex : '999'}}>
+            
+            <div className="text-center text-white">
+                    <h5>
+                        Copyright@ Invicon 2024
+                    </h5>
+            </div> 
+            </div>
+
+            )}
